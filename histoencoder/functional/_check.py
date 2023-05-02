a0aac138d015f3f6c51241b6c237b32a42b3c2ea
jopo666 <jopo@birdlover.com>
2023-05-02 18:03:37 +0300

    remove wrapper

--- histoencoder/functional/_check.py
+++ histoencoder/functional/_check.py
@@ -1,15 +1,9 @@
-from typing import Union
-
 from timm.models.xcit import XCiT
-
-from histoencoder._wrapper import HistoEncoder
 
 ERROR_NOT_XCIT = "Expected encoder to be XCiT model, got {}."
 
 
-def check_encoder(encoder: Union[XCiT, HistoEncoder]) -> XCiT:
-    if isinstance(encoder, HistoEncoder):
-        encoder = encoder.encoder
+def check_encoder(encoder: XCiT) -> XCiT:
     if not isinstance(encoder, XCiT):
         raise TypeError(ERROR_NOT_XCIT.format(type(encoder).__name__))
     return encoder
